b0d3d7f645bbe7e778c5a783fe22c50cabfffe52
sua <202445008@itc.ac.kr>
2024-10-04 11:53:22 +0900

    v0.8 .gitignore

--- week06/main.go
+++ week06/main.go
@@ -10,8 +10,8 @@ func main() {
 	var s string
 
 	fmt.Println(f, i, b, s)
-	fmt.Printf("%f%t%s%d\n", f, b, s, i)
-	f = 2.7 //이미 선언된 것에 단축 연산자 사용 X
+	fmt.Printf("%f%t%s%d\n", f, b, s, i) //zero value
+	f = 2.7                              //이미 선언된 것에 단축 연산자 사용 X
 	i = 3
-	fmt.Printf("\n\n", f > float64(i), "\n")
+	fmt.Print("\n\n", f > float64(i), "\n") //comparision (true/false)
 }
