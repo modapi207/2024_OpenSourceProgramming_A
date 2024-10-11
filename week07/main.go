81bdd53fa9c3e337379f15f4ebc993594568d52e
sua <202445008@itc.ac.kr>
2024-10-11 11:53:01 +0900

    v1.3 If

--- week07/main.go
+++ week07/main.go
@@ -5,15 +5,27 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	in := bufio.NewReader(os.Stdin)
-	fmt.Print("Input your name : ")
-	name, err := in.ReadString('\n')
+	fmt.Print("정수 입력 : ")
+	r := bufio.NewReader(os.Stdin)
+	i, err := r.ReadString('\n')
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	i = strings.TrimSpace(i)                  // python strip과 유사
+	score, err := strconv.ParseInt(i, 16, 32) // 문자열 변수 i 값을 정수형(32비트)으로 변환, 입력 받은 값은 16진수로 처리.
+
+	if score >= 60 {
+		fmt.Println("A")
+		fmt.Printf("%d\n", score)
 	} else {
-		fmt.Println(name)
-	} //nil값이 아니면 에러메세지가 있다는 뜻
+		fmt.Println("BCDF")
+		fmt.Printf("%d\n", score)
+	}
+
 }
