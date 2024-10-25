38a36b16301911c5c231198a52b84d01f08509f3
sua <202445008@itc.ac.kr>
2024-10-25 10:43:05 +0900

    v1.7 Guess Game

--- week07/main.go
+++ week07/main.go
@@ -18,7 +18,7 @@ func main() {
 	}
 
 	i = strings.TrimSpace(i)                  // python strip과 유사
-	score, err := strconv.ParseInt(i, 10, 32) // 문자열 변수 i 값을 정수형(32비트)으로 변환, 입력 받은 값은 16진수로 처리.
+	score, err := strconv.ParseInt(i, 16, 32) // 문자열 변수 i 값을 정수형(32비트)으로 변환, 입력 받은 값은 16진수로 처리.
 	var aOrNot string
 	if score >= 90 {
 		aOrNot = "A"
--- week09/main.go
+++ week09/main.go
@@ -1,13 +1,41 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"math/rand"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
 func main() {
 	rand.Seed(time.Now().Unix()) //42 같은 고정 값을 넣으면 안 돼
-	target := rand.Intn(6) + 1   //지금은 1~5
-	fmt.Printf("%d\n", target)
+	answer := rand.Intn(6) + 1   //지금은 1~5
+	fmt.Printf("%d\n", answer)
+
+	fmt.Print("숫자 입력 : ")
+	r := bufio.NewReader(os.Stdin)
+	i, err := r.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	i = strings.TrimSpace(i) // python strip과 유사
+	guess, err := strconv.Atoi(i)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(guess)
+
+	if answer == guess {
+		fmt.Println("정답입니다!")
+
+	} else if answer > guess {
+		fmt.Println("입력하신 수는 정답보다 작은 수입니다. LOW")
+	} else {
+		fmt.Println("입력하신 수는 정답보다 큰 수입니다. HIGH")
+	}
 }
