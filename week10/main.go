83a9a737aba194a678a08b98c0be2c7146fc211d
sua <202445008@itc.ac.kr>
2024-11-01 10:55:55 +0900

    v2.5 Prime Number

--- week10/main.go
+++ week10/main.go
@@ -22,13 +22,16 @@ func main() {
 	n, err := strconv.Atoi(i)
 	var isPrime bool = true //가독성, 저장공간크기 개선
 
-	for j := 2; j < n; j++ { // 2부터 입력된 수 앞까지 반복
-		if n%j == 0 { // 약수면
-			isPrime = false
-			//count++ //count=count+1 , 더하기 연산 제거
+	if n < 2 { //1보다 큰 자연수(**) 중 1과 자기 자신만을 약수로 가지는 수
+		isPrime = false
+	} else {
+		for j := 2; j < n; j++ { // 2부터 입력된 수 앞까지 반복
+			if n%j == 0 { // 약수면
+				isPrime = false
+				//count++ //count=count+1 , 더하기 연산 제거
+			}
 		}
 	}
-
 	//if counts==0 //나누어 떨어지는 수가 있으면 안 됨
 	if isPrime { //비교연산 제거
 		fmt.Printf("%d는(은) 소수입니다", n)
