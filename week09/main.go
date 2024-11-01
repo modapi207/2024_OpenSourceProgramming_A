c5f82425c9399039de70d71d2129be306d5cf7fd
sua <202445008@itc.ac.kr>
2024-11-01 10:51:46 +0900

    v2.4 Prime Number

--- week09/main.go
+++ week09/main.go
@@ -1,50 +1,18 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"math/rand"
-	"os"
-	"strconv"
-	"strings"
-	"time"
 )
 
 func main() {
-	rand.Seed(time.Now().Unix()) //42 같은 고정 값을 넣으면 안 돼
-	answer := rand.Intn(6) + 1   //지금은 1~5
-	//fmt.Printf("%d\n", answer) (정답 초반에 보여주는 거 지움)
+	var result string
+	result = fmt.Sprintf("%0.1f 나누기 %0.1f(은)는 %0.2f입니다.\n", 1.0, 3.0, 1.0/3.0)
+	fmt.Print(result)
 
-	win := false
-	for guesses := 0; guesses < 3; guesses++ {
-		fmt.Printf("%d번의 기회가 남았습니다. 숫자 입력 : ", 3-guesses)
-		r := bufio.NewReader(os.Stdin)
-		i, err := r.ReadString('\n')
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		i = strings.TrimSpace(i) // python strip과 유사
-		guess, err := strconv.Atoi(i)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(guess)
-
-		if answer == guess {
-			fmt.Println("정답입니다!")
-			win = true
-			break
-		} else if answer > guess {
-			fmt.Println("입력하신 수는 정답보다 작은 수입니다. LOW")
-		} else {
-			fmt.Println("입력하신 수는 정답보다 큰 수입니다. HIGH")
-		}
+	i := 1
+	for i <= 10 {
+		fmt.Printf("%5d\n", i)
+		i++
 	}
-	if win {
-		fmt.Println("당신이 이겼습니다!")
-	} else {
-		fmt.Printf("당신이 졌습니다. 정답은 %d입니다\n", answer)
-	}
+
 }
--- week10/main.go
+++ week10/main.go
@@ -20,16 +20,17 @@ func main() {
 	i = strings.TrimSpace(i)
 
 	n, err := strconv.Atoi(i)
-	counts := 0
-	//몇 번 나누어 떨어졌는지 기록하는 변수
+	var isPrime bool = true //가독성, 저장공간크기 개선
 
-	for j := 1; j <= n; j++ { // 1부터 입력된 수까지 반복
+	for j := 2; j < n; j++ { // 2부터 입력된 수 앞까지 반복
 		if n%j == 0 { // 약수면
-			counts++ // 나누어 떨어지는 횟수
+			isPrime = false
+			//count++ //count=count+1 , 더하기 연산 제거
 		}
 	}
 
-	if counts == 2 {
+	//if counts==0 //나누어 떨어지는 수가 있으면 안 됨
+	if isPrime { //비교연산 제거
 		fmt.Printf("%d는(은) 소수입니다", n)
 	} else {
 		fmt.Printf("%d는(은) 소수가 아닙니다", n)
