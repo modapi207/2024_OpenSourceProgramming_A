4867b9e2cba9c9e606ee47ddf0df158a1d76c922
sua <202445008@itc.ac.kr>
2024-11-08 11:52:30 +0900

    v3.5 Prime Number

--- week11/main.go
+++ week11/main.go
@@ -1,8 +1,43 @@
 package main
 
-import "week11/greeting"
+import (
+	"fmt"
+	"log"
+	"week11/keyboard"
+)
+
+func isPrime(n int) bool {
+	if n < 2 {
+		return false
+	} else if n == 2 {
+		return true
+	} else if n%2 == 0 {
+		return false
+	} else {
+		for j := 3; j*j <= n; j = j + 2 {
+			if n%j == 0 {
+				return false
+			}
+			//fmt.Printf("%d ", j)
+		}
+	}
+	return true
+}
 
 func main() {
-	greeting.Allgreetings("수아")
-
+	fmt.Print("첫 번째 정수(시작 값) 입력 : ")
+	n1, err := keyboard.GetInt()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Print("두 번째 정수(끝 값) 입력 : ")
+	n2, err := keyboard.GetInt()
+	if err != nil {
+		log.Fatal(err)
+	}
+	for i := n1; i <= n2; i++ {
+		if isPrime(i) {
+			fmt.Printf("%d ", i)
+		}
+	}
 }
