f1750a4d0174031c5f5ee9f69dd5fcc512b59f0c
sua <202445008@itc.ac.kr>
2024-11-15 10:52:21 +0900

    v3.7 array

--- week12/main.go
+++ week12/main.go
@@ -6,9 +6,15 @@ import (
 )
 
 func main() {
-	dates := [3]time.Time{time.Unix(1, 0),
+	dates := [3]time.Time{time.Unix(0, 0),
 		time.Unix(1447920000, 0),
-		time.Unix(1447920001, 0),
+		time.Unix(1447920001, 0), //쉼표가 반드시 필요
 	}
-	fmt.Println(dates[0], dates[1], dates[2])
+	//fmt.Println(dates[0], dates[1], dates[2])
+	//fmt.Println(dates) //배열 그 자체를 짝음
+	//fmt.Printf("%#v\n", dates) //어레이 리터럴 찍어봄.
+
+	for i := 0; i < len(dates); i++ {
+		fmt.Println(i, dates[i])
+	}
 }
