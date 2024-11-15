17abd8669ab17f5e2705c8cd42e7c56463ca2931
sua <202445008@itc.ac.kr>
2024-11-15 11:42:24 +0900

    v4.0 Array

--- week12/main.go
+++ week12/main.go
@@ -2,23 +2,20 @@ package main
 
 import (
 	"fmt"
-	"time"
+
+	"github.com/headfirstgo/keyboard"
 )
 
 func main() {
-	dates := [3]time.Time{time.Unix(0, 0),
-		time.Unix(1447920000, 0),
-		time.Unix(1447920001, 0), //쉼표가 반드시 필요
-	}
-	//fmt.Println(dates[0], dates[1], dates[2])
-	//fmt.Println(dates) //배열 그 자체를 짝음
-	//fmt.Printf("%#v\n", dates) //어레이 리터럴 찍어봄.
-	/*
-		for i := 0; i < len(dates); i++ {
-			fmt.Println(i, dates[i])
-		} */
+	//go get github.com/headfirstgo/keyboard
+	var gpa [3]float64
 
-	for i, v := range dates {
-		fmt.Println(i, v)
+	for i := 0; i < len(gpa); i++ {
+		fmt.Print("Input folat number")
+		gpa[i], _ = keyboard.GetFloat()
+	}
+
+	for index, value := range gpa {
+		fmt.Printf("%d %f\n", index, value)
 	}
 }
