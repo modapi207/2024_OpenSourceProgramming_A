0a57d6fa22f8ff05be99a9d5c3d063d9dfe12404
sua <202445008@itc.ac.kr>
2024-11-22 11:39:03 +0900

    v4.5 Slice

--- week13/main.go
+++ week13/main.go
@@ -7,7 +7,10 @@ import (
 func main() {
 
 	var emptySlice []int
-	emptySlice = make([]int, 5)
+	//emptySlice = make([]int, 5) //5개의 원소 생성
+	if len(emptySlice) == 0 {
+		emptySlice = append(emptySlice, 77)
+	}
 	fmt.Printf("%#v\n", emptySlice)
 
 	var gpa [5]float64 = [5]float64{3.5, 4.1, 4.5, 3.9, 4.23} //make array
