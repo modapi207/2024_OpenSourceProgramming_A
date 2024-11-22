530a623a108345bdc6c116845dde4b828abe81b3
sua <202445008@itc.ac.kr>
2024-11-22 11:30:04 +0900

    v4.3 Slice

--- week13/main.go
+++ week13/main.go
@@ -6,6 +6,10 @@ import (
 
 func main() {
 
+	var emptySlice []int
+	emptySlice = make([]int, 5)
+	fmt.Printf("%#v\n", emptySlice)
+
 	var gpa [5]float64 = [5]float64{3.5, 4.1, 4.5, 3.9, 4.23} //make array
 	gpa_slice := gpa[1:4]                                     //4.1, 4.5, 3.9
 	gpa[2] = 2.71
