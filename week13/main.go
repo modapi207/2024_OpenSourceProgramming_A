fb7604c210f820ae8381c5af8e792e140391d15e
sua <202445008@itc.ac.kr>
2024-11-22 11:28:00 +0900

    v4.2 Slice

---
+++ week13/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var gpa [5]float64 = [5]float64{3.5, 4.1, 4.5, 3.9, 4.23} //make array
+	gpa_slice := gpa[1:4]                                     //4.1, 4.5, 3.9
+	gpa[2] = 2.71
+	gpa_slice = append(gpa_slice, 4.3)
+	fmt.Println(len(gpa_slice), gpa_slice, gpa)
+
+}
