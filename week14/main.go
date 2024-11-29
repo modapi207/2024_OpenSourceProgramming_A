319445af50ecb7528c358243ead3d7a7981904af
sua <202445008@itc.ac.kr>
2024-11-29 11:56:54 +0900

    v4.9

--- week14/main.go
+++ week14/main.go
@@ -2,23 +2,43 @@ package main
 
 import "fmt"
 
-func main() {
+type visitor struct {
+	age  int
+	cost int
+}
 
-	type student struct {
-		id   int
-		name string
-		gpa  float32
+func calculateCost(visitors []visitor) int {
+	totalcost := 0
+	for _, v := range visitors {
+		totalcost = totalcost + v.cost
 	}
+	return totalcost
+} //공개 안 할 거니까 앞에 소문자 사용. visit이라는 구조체 타입.
 
-	var student1 student
-	student1.id = 20241234
-	student1.name = "JSA"
-	student1.gpa = 4.5
-	fmt.Println(student1.gpa)
+func main() {
+	var numVisitors int
+	fmt.Println("How many visitors? ")
+	fmt.Scanln(&numVisitors)
 
-	var student2 student
-	student2.id = 20244321
-	student2.name = "CJH"
-	student2.gpa = 4.7
-	fmt.Println(student2.gpa)
+	var vs []visitor                  //구조체 슬라이스
+	vs = make([]visitor, numVisitors) //숫자를 고정시키지 않으려 함.
+
+	for i := 0; i < numVisitors; i++ {
+		var age int
+		fmt.Print("Input age : ")
+		fmt.Scanln(&age)
+
+		switch {
+		case age < 12:
+			vs[i] = visitor{age: age, cost: 5000}
+
+		case age >= 12 && age < 65:
+			vs[i] = visitor{age: age, cost: 10000}
+
+		default: //65세 이상
+			vs[i] = visitor{age: age, cost: 7000}
+		} //if로 바꿔보기
+
+	} //슬라이스니까 index 활용
+	fmt.Printf("Total price is %dwon.", calculateCost(vs)) //구조체 받아옴
 }
